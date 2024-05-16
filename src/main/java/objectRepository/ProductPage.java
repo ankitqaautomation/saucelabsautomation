e090aa298289a8ae5cd27125f84be469708b3c75
ankitsharma <ankit.sharma@ontic.co>
2024-05-16 14:42:21 +0530

    handle exception

--- src/main/java/objectRepository/ProductPage.java
+++ src/main/java/objectRepository/ProductPage.java
@@ -43,62 +43,62 @@ public class ProductPage extends BaseClass {
 	private final By selectSortDropdown = By.xpath("//select[@data-test='product-sort-container']");
 
 	// add products to cart methods
-	public void addBagPackToCart() throws IOException {
+	public void addBagPackToCart()  {
 		getWebUtil().clickElement(addToCartButtonForBagPack);
 		printTestSteps("Click “Add to Cart” button for “Sauce Labs Backpack” on “Products” page");
 	}
 
-	public void addTshirtToCart() throws IOException {
+	public void addTshirtToCart()  {
 		getWebUtil().clickElement(addToCartButtonForTshirt);
 		printTestSteps("Click “Add to Cart” button for “Sauce Labs Bolt T-Shirt” on “Products” page");
 	}
 
-	public void addBikeLightToCart() throws IOException {
+	public void addBikeLightToCart()  {
 		getWebUtil().clickElement(addToCartButtonForBikeLight);
 		printTestSteps("Click “Add to Cart” button for “Sauce Labs Bike Light” on “Products” page");
 	}
 
-	public void addFleeceJacketToCart() throws IOException {
+	public void addFleeceJacketToCart()  {
 		getWebUtil().clickElement(addToCartButtonForFleeceJacket);
 		printTestSteps("Click “Add to Cart” button for “Sauce Labs Fleece Jacket” on “Products” page");
 	}
 
-	public void addOnesieToCart() throws IOException {
+	public void addOnesieToCart()  {
 		getWebUtil().clickElement(addToCartButtonForOnesie);
 		printTestSteps("Click “Add to Cart” button for “Sauce Labs Onesie” on “Products” page");
 	}
 
 	// validation methods
-	public void validateTshirtRemoveButtonAfterReset() throws IOException {
+	public void validateTshirtRemoveButtonAfterReset()  {
 		validateButtonNotVisibleforProduct(removeButtonForTshirt,
 				"Remove button is not visible for Sauce Labs Bolt T-Shirt");
 	}
 
-	public void validateTshirtAddButtonAfterReset() throws IOException {
+	public void validateTshirtAddButtonAfterReset()  {
 		validateButtonEnableForProduct(addToCartButtonForTshirt,
 				"Add to Cart button is enable for Sauce Labs Bolt T-Shirt");
 	}
 
-	public void validateFleeceJacketAddButtonNotVisible() throws IOException {
+	public void validateFleeceJacketAddButtonNotVisible()  {
 		validateButtonNotVisibleforProduct(addToCartButtonForFleeceJacket,
 				"Add to Cart button is not visible for for Sauce Labs Bolt Fleece Jacket");
 	}
 
-	public void validateOnesieAddButtonNotVisible() throws IOException {
+	public void validateOnesieAddButtonNotVisible()  {
 		validateButtonNotVisibleforProduct(addToCartButtonForOnesie,
 				"Add to Cart button is not visible for for Sauce Labs Bolt Onesie");
 	}
 
-	public void validateFleeceJacketRemoveButtonEnable() throws IOException {
+	public void validateFleeceJacketRemoveButtonEnable()  {
 		validateButtonEnableForProduct(removeButtonForFleeceJacket,
 				"Remove button is enable for Sauce Labs Fleece Jacket");
 	}
 
-	public void validateOnesieRemoveButtonEnable() throws IOException {
+	public void validateOnesieRemoveButtonEnable()  {
 		validateButtonEnableForProduct(removeButtonForOnesie, "Remove button is enable for Sauce Labs Onesie");
 	}
 
-	private void validateButtonNotVisibleforProduct(By by, String message) throws IOException {
+	private void validateButtonNotVisibleforProduct(By by, String message)  {
 		boolean isButtonVisible = true;
 		try {
 			getWebUtil().getElement(by).isDisplayed();
@@ -108,7 +108,7 @@ public class ProductPage extends BaseClass {
 		validate(isButtonVisible, message, false);
 	}
 
-	private void validateButtonEnableForProduct(By by, String message) throws IOException {
+	private void validateButtonEnableForProduct(By by, String message)  {
 		boolean isButtonVisible = false;
 		try {
 			isButtonVisible = getWebUtil().getElement(by).isEnabled();
@@ -118,22 +118,22 @@ public class ProductPage extends BaseClass {
 
 	}
 
-	public void validateEmptyCart() throws IOException {
+	public void validateEmptyCart()  {
 		validate(getCartCount().isEmpty(), "Cart is shown Empty", true);
 	}
 
-	public void validateSeletedSortOption(String option) throws IOException {
+	public void validateSeletedSortOption(String option)  {
 		validate(getWebUtil().getText(selectedSortOption).equalsIgnoreCase(option),
 				"Assert if the Selected (Displayed) Item on the Product Sort is “" + option + "”", true);
 	}
 
-	public void validateCartValue(String value) throws IOException {
+	public void validateCartValue(String value)  {
 		printTestSteps("Capture value from “Cart Icon” on the top right and assert is its “" + value + "”");
 		validate(getCartCount().equalsIgnoreCase(value), "Assert the cart quantity value", true);
 
 	}
 
-	public void validatePriceSortedOrder() throws IOException {
+	public void validatePriceSortedOrder()  {
 		List<WebElement> priceElements = getWebUtil().getElementsList(priceOfAllProductLocator);
 		List<Double> prices = new ArrayList<>();
 		for (WebElement priceElement : priceElements){
@@ -148,29 +148,29 @@ public class ProductPage extends BaseClass {
 	}
 
 	// remove product methods
-	public void removeBagPackFromCart() throws IOException {
+	public void removeBagPackFromCart()  {
 		getWebUtil().clickElement(removeButtonForBagPack);
 		printTestSteps("Click “Remove” button for “Sauce Labs Backpack” on “Products” page.");
 	}
 
 	// general page methods
-	public void goToCart() throws IOException {
+	public void goToCart()  {
 		getWebUtil().clickElement(cartIcon);
 		printTestSteps("Click “Cart” icon on “Products” page");
 
 	}
 
-	public String getProductPriceFromProductPage(String productName) throws IOException {
+	public String getProductPriceFromProductPage(String productName)  {
 		printTestSteps("Capture price of “" + productName + "” from “Products” page");
 		return getWebUtil().getText(By.xpath("//div[text()='" + productName
 				+ "']/ancestor::div[@data-test='inventory-item-description']//div[@data-test='inventory-item-price']"));
 	}
 
-	private String getCartCount() throws IOException {
+	private String getCartCount()  {
 		return getWebUtil().getText(cartIcon);
 	}
 
-	public void selectLowHighFilter(String option) throws IOException {
+	public void selectLowHighFilter(String option)  {
 
 		getWebUtil().clickElement(selectSortDropdown);
 
